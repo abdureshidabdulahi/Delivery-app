1b676ef3cb10bc52ca2fca54b8c7284c25fb2f8a
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-12-21 21:35:43 +0300

    update

--- delivery/src/components/exploreMenu/exploreMenu.jsx
+++ delivery/src/components/exploreMenu/exploreMenu.jsx
@@ -5,7 +5,19 @@ export default function ExploreMenu(){
     return(
         <div className="exploremenu" id='explore-menu'>
             <h1>Explore Our Menu</h1>
-            <p className='explore-menu-test'></p>
+            <p className='explore-menu-text'>
+            choose from a diverse menu featuring delectable array of dishes crafted with the 
+            finest ingredients and culinary expertise our mission is to satisfy your cravings and elevate
+            your dining experience,one delicious meal at a timer
+            </p>
+            <div className="explore-menu-list">
+                {menu_list.map((item,index)=>(
+                    <div key={index} className='explore-menu-list-item'>
+                       <img src={item.menu_image} alt="" />
+                       <p>{item.menu_name}</p>
+                    </div>
+                ))}
+            </div>
 
         </div>
     )
--- delivery/src/components/pages/home/home.jsx
+++ delivery/src/components/pages/home/home.jsx
@@ -1,10 +1,12 @@
  
 import Header from '../../header/header'
 import './home.css'
+import ExploreMenu from '../../exploreMenu/exploreMenu'
 export  default function Home(){
     return(
         <div className='home-app'>
           <Header/>
+          <ExploreMenu/>
         </div>
     )
 }
