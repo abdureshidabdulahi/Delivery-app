f5cc8321f35045c064e1c42cdd96e9ea0dca2738
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-12-21 09:52:13 +0300

    update

--- delivery/src/components/navbar/navbar.js
+++ delivery/src/components/navbar/navbar.js
@@ -1,10 +1,28 @@
-import React from 'react'
+ import { assets } from '../../food-del-assets/assets/frontend_assets/assets';
+import './navbar.css';
 
 const navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+    <img src={assets.logo} className='logo'  alt=''/>
+    <ul className='navbar-menu'>
+      <li>home</li>
+       <li>menu</li>
+        <li>mobile-app</li>
+         <li>contact us</li>
+    
+    </ul>
+    <div className='navbar-menu-right'>
+      <img src={assets.search_icon} className='search-icon' alt=''/>
+      <div>
+        <img src={assets.basket_icon} alt=''/>
+        <div className='dot'></div>
+         <button>sign in</button>
+      </div>
+   
     </div>
+
+    </div> 
   )
 }
 
