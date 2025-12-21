27f82ba30e0bdfd0bb764c92aa6d4b781f0a00b2
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-12-21 19:30:22 +0300

    update

---
+++ delivery/src/components/exploreMenu/exploreMenu.jsx
@@ -0,0 +1,12 @@
+
+import './exploreMenu.css'
+import { menu_list } from '../../food-del-assets/assets/frontend_assets/assets'
+export default function ExploreMenu(){
+    return(
+        <div className="exploremenu" id='explore-menu'>
+            <h1>Explore Our Menu</h1>
+            <p className='explore-menu-test'></p>
+
+        </div>
+    )
+}
