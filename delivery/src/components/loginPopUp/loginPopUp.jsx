01c0f32336834bfbefd3ad23fed6827dd24048bc
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-01-06 21:55:47 +0300

    update

--- delivery/src/components/loginPopUp/loginPopUp.jsx
+++ delivery/src/components/loginPopUp/loginPopUp.jsx
@@ -3,7 +3,18 @@ import { assets } from "../../food-del-assets/assets/frontend_assets/assets"
 import './loginPopUp.css'
  
 const LoginPopUp = ({setShowLogin}) => {
-  const [currentState,setCurentState] = useState(' Sign Up')
+  const [currentState,setCurentState] = useState('Login')
+  const [data,setData] = useState({
+    name:'',
+    email:'',
+    password:''
+  })
+  const onchangeHandler = (event)=>{
+      const name = event.target.name
+      const value = event.target.value
+      setData(data=>({...data,[name]:value}))
+  }
+ 
   return (
     <div className="login-popup">
        <form action="" className="login-popup-container">
@@ -12,9 +23,9 @@ const LoginPopUp = ({setShowLogin}) => {
           <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
         <div className="login-popup-inputs">
-          {currentState==='Login'?'':<input type="text" placeholder="your name" required/>}
-          <input type="email" placeholder="your email" />
-          <input type="password" placeholder="password" required/>
+          {currentState==='Login'?'':<input name="name" onChange={onchangeHandler} value={data.name} type="text" placeholder="your name" required/>}
+          <input name="email" value={data.email} onChange={onchangeHandler} type="email" placeholder="your email" />
+          <input name="password" value={data.password} onChange={onchangeHandler} type="password" placeholder="password" required/>
         </div>
         <button> {currentState==='Sign Up'?'Create account':'Login'} </button>
         <div className="login-popup-condition">
@@ -22,7 +33,7 @@ const LoginPopUp = ({setShowLogin}) => {
           <p>By continuing, i agree to the terms of use and privacy policy.</p>
         </div>
         {currentState==='Login'
-        ?<p>Create a new account? <span onClick={()=>setCurentState('Sign Up')}>Click here</span></p>
+        ?<p>Create a new account? <span onClick={()=>setCurentState('Sign Up')}>Signin here</span></p>
         :<p>Already have an account? <span onClick={()=>setCurentState('Login')}>Login here</span></p>
       }   
        </form>
