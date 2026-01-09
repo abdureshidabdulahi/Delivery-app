33f340c716c6f9b3533b5b455ed40b2c3e249ad7
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-01-09 11:39:34 +0300

    update

--- backend/middleWare/auth.js
+++ backend/middleWare/auth.js
@@ -1,7 +1,20 @@
 import jwt from 'jsonwebtoken'
 
 const authMiddleWare =async (req,res,calback)=>{
+            const {token} = req.headers
+            if(!token){
+                return res.json({success:false,message:'notAuth login again'})
+            }
+            try{
+                const token_decode =jwt.verify(token,process.env.JWT_SECRET);
+                req.body.userId = token_decode.id
+                next()
 
+            }catch(err){
+                console.log(err)
+                res.json({success:false,message:'error inauth'})
+
+            }
 
 }
 
