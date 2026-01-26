c2e252027e310914ce4e4ae957017a7e31202f51
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-01-26 14:42:29 +0300

    update

---
+++ backend/controllers/orderControler.js
@@ -0,0 +1,11 @@
+ import orderModel from "../models/orderModel";
+ import userModel from '../models/userModel.js';
+  import Stripe from 'stripe'
+
+
+
+  //placing user order from frontend
+  const  placeOrder = async (req,res)=>{
+
+  }
+
